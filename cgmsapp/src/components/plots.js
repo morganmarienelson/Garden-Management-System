03ce814ad724c5193ed5a97454c18741e976ad06
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2022-10-04 11:31:26 -0500

    OpenBtnApp: fixed issue CRF-61

--- cgmsapp/src/components/plots.js
+++ cgmsapp/src/components/plots.js
@@ -1,9 +1,11 @@
 import React from "react";
+import OpenAppBtn from "./openAppBtn";
 
 export default function Plots() {
     return (
         <div>
         <h1>Plots</h1>
+            <OpenAppBtn/>
         </div>
     );
     };
