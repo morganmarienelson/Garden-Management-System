b207f6ced8e3116b16eae3b421a83b04c596d411
Enock619 <enock619.eo@gmail.com>
2022-10-04 09:41:14 -0500

    Create state and sample data

---
+++ cgmsapp/src/components/ApplicantsState.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Applicants = () => {
+  return [
+    {
+      name: "Jason",
+      address: "1234",
+      number: "233-305-0000",
+      plotSize: "20 X 20",
+      email: "Jason123@gmail.com",
+    },
+    {
+      name: "Emily",
+      address: "4321",
+      number: "800 700 6000",
+      plotSize: "30 X 20",
+      email: "emily12@gmail.com",
+    },
+  ];
+};
+
+export default Applicants;
