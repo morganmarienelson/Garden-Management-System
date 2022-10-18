544568687570f3eaf67eb220ac8186ab861e9fb2
Braden Konop <55826123+bkonop@users.noreply.github.com>
2022-10-18 10:42:44 -0500

    fixed columns after my merge request messed them up.

--- cgmsapp/src/components/PlotGrid.js
+++ cgmsapp/src/components/PlotGrid.js
@@ -8,25 +8,49 @@ import DeletePlotsBtn from './DeletePlotsBtn';
 class PlotGrid extends React.Component {
   constructor(props) {
     super(props);
-    const cols =  [
-      { field: 'id', headerName: 'ID', width: 70, editable: false},
+    const cols = [
+      { field: 'id', headerName: 'ID', width: 100, editable: false},
       {
         field: 'dimensions',
         headerName: 'Dimensions',
-        width: 150,
-        editable: false,
-      },
-      {
-        field: 'owner',
-        headerName: 'Owner',
-        width: 150,
+        width: 100,
         editable: false,
       },
       {
         field: 'feeAmount',
         headerName: 'Yearly Fee',
-        width: 150,
-        editable: false,
+        width: 100,
+        editable: true,
+      },
+      {
+        field: 'vacant',
+        headerName: 'Vacant',
+        width: 100,
+        editable: true,
+      },
+      {
+        field: 'owner firstName',
+        headerName: 'First Name',
+        width: 100,
+        editable: true,
+      },
+      {
+        field: 'owner lastName',
+        headerName: 'Last Name',
+        width: 100,
+        editable: true,
+      },
+      {
+        field: 'owner email',
+        headerName: 'Email',
+        width: 100,
+        editable: true,
+      },
+      {
+        field: 'owner phone',
+        headerName: 'Phone',
+        width: 100,
+        editable: true,
       },
       {
         field: 'other',
