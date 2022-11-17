ff9caa8aba02b2e2f59f60f57775b05a937f4d19
morganmarienelson <87725126+morganmarienelson@users.noreply.github.com>
2022-11-17 10:22:35 -0600

    blank conversations cannot be created CRF-225

--- cgmsapp/src/components/directMessagingSystem/newConversationModal.js
+++ cgmsapp/src/components/directMessagingSystem/newConversationModal.js
@@ -2,16 +2,29 @@ import React, {useState} from "react"
 import {Modal, Form, Button} from 'react-bootstrap'
 import { useContacts } from "../../context/ContactsProvider"
 import { useConversations } from "../../context/ConversationsProvider"
+import {Alert, Snackbar} from "@mui/material";
+
 
 export default function NewConversationModal({closeModal}) {
     const [selectedContactUsernames, setSelectedContactUsernames] = useState([]);
     const { contacts } = useContacts();
     const {createConversation} = useConversations();
+    const [alertOpen, setAlertOpen] = useState(false);
+    const [message, setMessage] = useState("");
 
     function handleSubmit(e){
         e.preventDefault();
-         createConversation(selectedContactUsernames)
-        closeModal();
+        if (selectedContactUsernames.length === 0){
+            setMessage('Contact not selected')
+            setAlertOpen(true)
+        } else {
+            createConversation(selectedContactUsernames)
+            closeModal();
+        }
+    }
+
+    const handleAlertClose = () =>{
+        setAlertOpen(false);
     }
 
     function handleCheckboxChange(contactUsername){
@@ -43,6 +56,11 @@ export default function NewConversationModal({closeModal}) {
                     <Button type="submit" className="m-2">Create</Button>
                 </Form>
             </Modal.Body>
+            <Snackbar open={alertOpen} sx={{ width: '100%' }} autoHideDuration={2000} onClose={handleAlertClose}>
+                <Alert autoHideDuration={1} severity="error" onClose={handleAlertClose}>
+                    {message}
+                </Alert>
+            </Snackbar>
         </>
 
     );
